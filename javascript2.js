f6094cabbc98bace1357f662b459e07f72235161
Max <57727195+mafxs@users.noreply.github.com>
2023-10-11 22:47:13 +0100

    javascript2 func examples, test3 new file

--- javascript2.js
+++ javascript2.js
@@ -1,7 +1,31 @@
+function agechecker (age) {
+  if (age > 20 && age < 100) {
+    return true;
+  } else if (age > 100) {
+    return;
+  } else {
+    return false;
+  }
+}
 
-console.log();
+//let age = prompt('how old are you? :)');
+
+//if (agechecker(age)) {
+//  console.log('old');
+//} else if (agechecker(age) == undefined) {
+//  console.log('way too old?')
+//} else {
+//  console.log('too young');
+//}
 
 
 
+function checkAge(age) {
+  if ((age > 18) ? true : confirm('Did parents allow you?')) {
+    return true;
+  }
+  return console.log('not getting through');
+}
 
-  
+let age = 13;
+console.log(checkAge(age));
